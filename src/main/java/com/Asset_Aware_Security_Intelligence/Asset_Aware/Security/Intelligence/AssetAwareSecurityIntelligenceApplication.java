9761c806d56c74a44f05295571f68da79940d076
mAliz007 <m.ali.zafar2005@gmail.com>
2026-04-09 18:22:31 +0500

    Initial Project Setup

---
+++ src/main/java/com/Asset_Aware_Security_Intelligence/Asset_Aware/Security/Intelligence/AssetAwareSecurityIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.Asset_Aware_Security_Intelligence.Asset_Aware.Security.Intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetAwareSecurityIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetAwareSecurityIntelligenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Asset_Aware_Security_Intelligence/Asset_Aware/Security/Intelligence/AssetAwareSecurityIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Asset_Aware_Security_Intelligence.Asset_Aware.Security.Intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetAwareSecurityIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
